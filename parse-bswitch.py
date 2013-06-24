95baacb5159d16b34b025cfd2b08dd2028b3f935
Jordan <jordan@Elf.(none)>
2013-06-24 09:55:18 -0700

    Added logic to disaggregate response times still further, by switch-n/nonswitch-n status

--- parse-bswitch.py
+++ parse-bswitch.py
@@ -1,14 +1,14 @@
 import codecs
 import numpy
 filelist = open('./bswitch-filelist')
-summary = [['Filename','Subject', '%ACC-switch', 'Mean RT-ACC_switch', 'Mean RT-INACC_switch', '%ACC-nonswitch', 'Mean RT-ACC-nonswitch', 'Mean RT-INACC_nonswitch', '%ACC-total', 'Mean RT-ACC_total', 'Mean RT-INACC_total']]
+summary = [['Filename','Subject', '%ACC-total', 'Mean RT-ACC_total', 'Mean RT-INACC_total', '%ACC-all_nonswitch', 'Mean RT-ACC-all_nonswitch', 'Mean RT-INACC_all_nonswitch', '%ACC-all_switch', 'Mean RT-ACC_all_switch', 'Mean RT-INACC_all_switch', '%ACC-nonswitch0', 'Mean RT-ACC-nonswitch0', 'Mean RT-INACC_nonswitch0', '%ACC-nonswitch1', 'Mean RT-ACC-nonswitch1', 'Mean RT-INACC_nonswitch1', '%ACC-nonswitch2', 'Mean RT-ACC-nonswitch2', 'Mean RT-INACC_nonswitch2', '%ACC-nonswitch3', 'Mean RT-ACC-nonswitch3', 'Mean RT-INACC_nonswitch3', '%ACC-switch0', 'Mean RT-ACC_switch0', 'Mean RT-INACC_switch0' '%ACC-switch1', 'Mean RT-ACC_switch1', 'Mean RT-INACC_switch1', '%ACC-switch2', 'Mean RT-ACC_switch2', 'Mean RT-INACC_switch2', '%ACC-switch3', 'Mean RT-ACC_switch3', 'Mean RT-INACC_switch3', '%ACC-switch4', 'Mean RT-ACC_switch4', 'Mean RT-INACC_switch4']]
 
 for filename in filelist:
         print "Now processing " + filename.strip()
         datafile = codecs.open(filename.strip(),encoding='utf16')
 	datarows = []
 	rts = []
-	datarowsbycase = {'switch':[], 'nonswitch':[], 'all':[]}
+	datarowsbycase = {'all_switch':[], 'switch0':[], 'switch1':[], 'switch2':[], 'switch3':[], 'switch4':[] , 'all_nonswitch':[], 'nonswitch0':[], 'nonswitch1':[], 'nonswitch2':[], 'nonswitch3':[], 'all':[]}
         for row in datafile:
                 if row.count(',') < 80:
                         continue
@@ -43,7 +43,7 @@ for filename in filelist:
 	print replacerts
 
 	#print datarows
-	priorstim = datarows[0].strip().split(',')[1]
+	priorstim = [datarows[0].strip().split(',')[1], 0, 0] #Switch status marker: [0]: case of last stim; [1]: switch depth; [2]: non-switch depth
 	#print priorstim
 	for i in range(0, len(datarows)):
 		activerow = datarows[i].strip().split(',')
@@ -51,23 +51,34 @@ for filename in filelist:
 			if int(activerow[3]) == rt[0]:
 				activerow[3] = unicode(str(rt[1]))
 		datarowsbycase['all'].append(activerow)
-		if activerow[1]  == priorstim:
-			datarowsbycase['nonswitch'].append(activerow)
+		if activerow[1] == priorstim[0]:
+			datarowsbycase['all_nonswitch'].append(activerow)
+			datarowsbycase['nonswitch'+str(priorstim[2])].append(activerow)
+			priorstim[1] = 0
+			priorstim[2] += 1
+			
 		else:
-			datarowsbycase['switch'].append(activerow)
-			priorstim = activerow[1]
-
+			datarowsbycase['all_switch'].append(activerow)
+			datarowsbycase['switch'+str(priorstim[1])].append(activerow)
+			priorstim[0] = activerow[1]
+			priorstim[1] += 1
+			priorstim[2] = 0
+			
 	#print datarowsbycase
 	filereport = [filename[filename.rfind('/')+1:].strip(), datarows[0].strip().split(',')[0]]
-	for case, dataarray in datarowsbycase.iteritems():
+	for case, dataarray in iter(sorted(datarowsbycase.iteritems())):
 		correctrts = [int(datapoint[3]) for datapoint in dataarray if datapoint[2] == '1']
 		wrongrts = [int(datapoint[3]) for datapoint in dataarray if datapoint[2] == '0']
 		#print correctrts
-		print "% accuracy for " + case + " trials: " + str(float(len(correctrts))/float(len(dataarray)))
-		print "% inaccuracy for " + case + " trials: " + str(float(len(wrongrts))/float(len(dataarray)))
-		#print "Average response time for correct answers for " + case + " trials: " + str(numpy.mean(correctrts))
+		numtrials = len(dataarray)
+		print "Number of " + case + " trials: " + str(numtrials)
+		if numtrials == 0:
+			numtrials = 1
+		print "% accuracy for " + case + " trials: " + str(float(len(correctrts))/float(numtrials))
+		#print "% inaccuracy for " + case + " trials: " + str(float(len(wrongrts))/float(len(dataarray)))
+		print "Average response time for correct answers for " + case + " trials: " + str(numpy.mean(correctrts))
 		print "Average response time for wrong answers for " + case + " trials: " + str(numpy.mean(wrongrts))
-		filereport.append(str(float(len(correctrts))/float(len(dataarray))))
+		filereport.append(str(float(len(correctrts))/float(numtrials)))
 		filereport.append(str(numpy.mean(correctrts)))
 		filereport.append(str(numpy.mean(wrongrts)))
 #	activerow = ','.join(activerow)
@@ -81,6 +92,9 @@ for filename in filelist:
         summary.append(filereport)
 report = codecs.open('./bswitch-report.csv','w',encoding='utf16')
 for row in summary:
+#	for entry in row:
+#		if entry.find('nan') > 0:
+#			entry = 'NULL'
         report.write(','.join(row) + "\n")
 report.write("\n")
 report.close();
