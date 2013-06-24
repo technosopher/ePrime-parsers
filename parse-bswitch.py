018bfabfeb53ef74e30eb18680c95e5e7c40e124
Jordan <jordan@Elf.(none)>
2013-06-24 10:00:03 -0700

    Decomposed outlier check in both scripts into its own function, and moved to a percentage-based definition of how many items to check for outlier status

--- parse-bswitch.py
+++ parse-bswitch.py
@@ -1,55 +1,75 @@
 import codecs
 import numpy
 filelist = open('./bswitch-filelist')
-summary = [['Filename','Subject', '%ACC-total', 'Mean RT-ACC_total', 'Mean RT-INACC_total', '%ACC-all_nonswitch', 'Mean RT-ACC-all_nonswitch', 'Mean RT-INACC_all_nonswitch', '%ACC-all_switch', 'Mean RT-ACC_all_switch', 'Mean RT-INACC_all_switch', '%ACC-nonswitch0', 'Mean RT-ACC-nonswitch0', 'Mean RT-INACC_nonswitch0', '%ACC-nonswitch1', 'Mean RT-ACC-nonswitch1', 'Mean RT-INACC_nonswitch1', '%ACC-nonswitch2', 'Mean RT-ACC-nonswitch2', 'Mean RT-INACC_nonswitch2', '%ACC-nonswitch3', 'Mean RT-ACC-nonswitch3', 'Mean RT-INACC_nonswitch3', '%ACC-switch0', 'Mean RT-ACC_switch0', 'Mean RT-INACC_switch0' '%ACC-switch1', 'Mean RT-ACC_switch1', 'Mean RT-INACC_switch1', '%ACC-switch2', 'Mean RT-ACC_switch2', 'Mean RT-INACC_switch2', '%ACC-switch3', 'Mean RT-ACC_switch3', 'Mean RT-INACC_switch3', '%ACC-switch4', 'Mean RT-ACC_switch4', 'Mean RT-INACC_switch4']]
+summary = [['Filename','Subject', '%ACC-total', 'Mean RT-ACC_total', 'Mean RT-INACC_total', '%ACC-all_nonswitch', 'Mean RT-ACC-all_nonswitch', 'Mean RT-INACC_all_nonswitch', '%ACC-all_switch', 'Mean RT-ACC_all_switch', 'Mean RT-INACC_all_switch', '%ACC-nonswitch0', 'Mean RT-ACC-nonswitch0', 'Mean RT-INACC_nonswitch0', '%ACC-nonswitch1', 'Mean RT-ACC-nonswitch1', 'Mean RT-INACC_nonswitch1', '%ACC-nonswitch2', 'Mean RT-ACC-nonswitch2', 'Mean RT-INACC_nonswitch2', '%ACC-nonswitch3', 'Mean RT-ACC-nonswitch3', 'Mean RT-INACC_nonswitch3', '%ACC-switch0', 'Mean RT-ACC_switch0', 'Mean RT-INACC_switch0', '%ACC-switch1', 'Mean RT-ACC_switch1', 'Mean RT-INACC_switch1', '%ACC-switch2', 'Mean RT-ACC_switch2', 'Mean RT-INACC_switch2', '%ACC-switch3', 'Mean RT-ACC_switch3', 'Mean RT-INACC_switch3', '%ACC-switch4', 'Mean RT-ACC_switch4', 'Mean RT-INACC_switch4']]
+
+def id_outliers(list_of_RTs, check_depth, max_step): #Takes a list of response times, an int indicating how many RTs on either end of the sorted list should be evaluated, and an int indicating how large of a jump between two sequential RTs in these end regions designates the start of the outliers.  
+	if check_depth < 2:
+		return []
+	list_of_RTs.sort()
+	replace_RTs = []
+	trigger = 0
+	print list_of_RTs[:check_depth]
+	print list_of_RTs[-check_depth:]
+	for i in range(len(list_of_RTs[:check_depth])-1,-1,-1):
+		if trigger > 0:
+			replace_RTs.append([list_of_RTs[:check_depth][i],trigger])
+		elif i < 0:
+			break	
+		elif list_of_RTs[:check_depth][i]-list_of_RTs[:check_depth][i-1] > max_step:
+			trigger = list_of_RTs[:check_depth][i]
+	trigger = 0
+	for i in range(len(list_of_RTs[-check_depth:])):
+		if trigger > 0:
+			replace_RTs.append([list_of_RTs[-check_depth:][i],trigger])
+		elif i == len(list_of_RTs[-check_depth:])-1:
+			break
+		elif list_of_RTs[-check_depth:][i+1]-list_of_RTs[-check_depth:][i] > max_step:
+			trigger = list_of_RTs[-check_depth:][i]
+	print replace_RTs
+	return replace_RTs #Returns a list of pairs of [Original RTs, Replacement RTs]
 
 for filename in filelist:
         print "Now processing " + filename.strip()
         datafile = codecs.open(filename.strip(),encoding='utf16')
 	datarows = []
-	rts = []
+	rts_acc = []
+	rts_inacc = []
 	datarowsbycase = {'all_switch':[], 'switch0':[], 'switch1':[], 'switch2':[], 'switch3':[], 'switch4':[] , 'all_nonswitch':[], 'nonswitch0':[], 'nonswitch1':[], 'nonswitch2':[], 'nonswitch3':[], 'all':[]}
         for row in datafile:
                 if row.count(',') < 80:
                         continue
 		activerow = row.strip().split(',')
 		if activerow[82] == '1' or activerow[82] == '0':
-			rts.append(int(activerow[89]))
+			if activerow[82] == '1':
+				rts_acc.append(int(activerow[89]))
+			else:
+				rts_inacc.append(int(activerow[89]))
 			activerow = activerow[1]+','+activerow[72]+','+activerow[82]+','+activerow[89] #Subject, Cue Type (L/N), If Correct, Response Time
 			datarows.append(activerow)
-
-	rts.sort()
-	replacerts = []
-	trigger = 0
-	for i in range(len(rts[:10])-1,-1,-1):
-		if trigger > 0:
-			replacerts.append((rts[:10][i],trigger))
-		elif i == 0:
-			break	
-		elif rts[:10][i]-rts[:10][i-1] > 100:
-			trigger = rts[:10][i]
-	trigger = 0
-	for i in range(len(rts[-10:])):
-		if trigger > 0:
-			replacerts.append((rts[-10:][i],trigger))
-		elif i == len(rts[-10:])-1:
-			break
-		elif rts[-10:][i+1]-rts[-10:][i] > 100:
-			trigger = rts[-10:][i]
-
-	print rts[:10]
-	print rts[-10:]
+	print len(datarows)
+	print len(rts_acc)
+	print len(rts_inacc)
+	replacerts_acc = id_outliers(rts_acc, len(rts_acc)/10, 200) 
+	replacerts_inacc = id_outliers(rts_inacc, len(rts_inacc)/5, 200) 
 	print "Replacing"
-	print replacerts
+	print replacerts_acc
+	print replacerts_inacc
 
 	#print datarows
 	priorstim = [datarows[0].strip().split(',')[1], 0, 0] #Switch status marker: [0]: case of last stim; [1]: switch depth; [2]: non-switch depth
 	#print priorstim
 	for i in range(0, len(datarows)):
 		activerow = datarows[i].strip().split(',')
-		for rt in replacerts:
-			if int(activerow[3]) == rt[0]:
-				activerow[3] = unicode(str(rt[1]))
+		if activerow[2] == '1':
+			for rt in replacerts_acc:
+				if int(activerow[3]) == rt[0]:
+					activerow[3] = unicode(str(rt[1]))
+		else:
+			for rt in replacerts_inacc:
+				if int(activerow[3]) == rt[0]:
+					activerow[3] = unicode(str(rt[1]))
+			
 		datarowsbycase['all'].append(activerow)
 		if activerow[1] == priorstim[0]:
 			datarowsbycase['all_nonswitch'].append(activerow)
@@ -71,23 +91,15 @@ for filename in filelist:
 		wrongrts = [int(datapoint[3]) for datapoint in dataarray if datapoint[2] == '0']
 		#print correctrts
 		numtrials = len(dataarray)
-		print "Number of " + case + " trials: " + str(numtrials)
+		#print "Total number of " + case + " responses: " + str(numtrials)
+		print "Number of correct " + case + " responses: " + str(len(correctrts))
+		#print "Number of incorrect " + case + " responses: " + str(len(wrongrts))
 		if numtrials == 0:
-			numtrials = 1
-		print "% accuracy for " + case + " trials: " + str(float(len(correctrts))/float(numtrials))
-		#print "% inaccuracy for " + case + " trials: " + str(float(len(wrongrts))/float(len(dataarray)))
-		print "Average response time for correct answers for " + case + " trials: " + str(numpy.mean(correctrts))
-		print "Average response time for wrong answers for " + case + " trials: " + str(numpy.mean(wrongrts))
+			numtrials = 1 #To deal with errors thrown by dividing zero by zero
 		filereport.append(str(float(len(correctrts))/float(numtrials)))
 		filereport.append(str(numpy.mean(correctrts)))
 		filereport.append(str(numpy.mean(wrongrts)))
-#	activerow = ','.join(activerow)
-#	print activerow
-# for case, rts in rtsbycase.iteritems():
-#	print "Mean for "+case+" is: "+ str(numpy.mean(rts))
-#	print rts
-#	print "Size: "+str(len(rts))
-	print filereport
+#	print filereport
         datafile.close()
         summary.append(filereport)
 report = codecs.open('./bswitch-report.csv','w',encoding='utf16')
--- parse-filtering.py
+++ parse-filtering.py
@@ -1,59 +1,77 @@
 import codecs
 import numpy
 filelist = open('./filtering-filelist')
-summary = [['Filename','Subject', '2_0 RT', '2_2 RT', '2_4 RT', '2_6 RT']]
+summary = [['Filename','Subject', 'Acc_total', 'RT_2_0',  'Acc_2_0', 'RT_2_2', 'Acc_2_2', 'RT_2_4', 'Acc_2_4', 'RT_2_6', 'Acc_2_6']]
+
+def id_outliers(list_of_RTs, check_depth, max_step): #Takes a list of response times, an int indicating how many RTs on either end of the sorted list should be evaluated, and an int indicating how large of a jump between two sequential RTs in these end regions designates the start of the outliers.  
+        if check_depth < 2:
+                check_depth = 2
+                if check_depth > len(list_of_RTs)/2:
+                        return []
+        list_of_RTs.sort()
+        replace_RTs = []
+        trigger = 0
+        #print list_of_RTs[:check_depth]
+        #print list_of_RTs[-check_depth:]
+        for i in range(len(list_of_RTs[:check_depth])-1,-1,-1):
+                if trigger > 0:
+                        replace_RTs.append([list_of_RTs[:check_depth][i],trigger])
+                elif i < 0:
+                        break
+                elif list_of_RTs[:check_depth][i]-list_of_RTs[:check_depth][i-1] > max_step:
+                        trigger = list_of_RTs[:check_depth][i]
+        trigger = 0
+        for i in range(len(list_of_RTs[-check_depth:])):
+                if trigger > 0:
+                        replace_RTs.append([list_of_RTs[-check_depth:][i],trigger])
+                elif i == len(list_of_RTs[-check_depth:])-1:
+                        break
+                elif list_of_RTs[-check_depth:][i+1]-list_of_RTs[-check_depth:][i] > max_step:
+                        trigger = list_of_RTs[-check_depth:][i]
+        print replace_RTs
+        return replace_RTs #Returns a list of pairs of [Original RTs, Replacement RTs]
+
 for filename in filelist:
 	print "Now processing " + filename.strip()
 	datafile = codecs.open(filename.strip(),encoding='utf16')
 	datarows = []
-	rts = []
+	rts_acc = []
+	rts_inacc = []
 	rtsbycase = {'2_0':[], '2_2':[], '2_4':[], '2_6':[]}
 	for row in datafile:
 		if row.count(',') < 50:
 			continue
 		activerow = row.strip().split(',')
-		if activerow[52] == '1':
-			if activerow[41].find("2_") > -1:
-				rts.append(int(activerow[59]))
-				activerow = activerow[1]+','+activerow[41]+','+activerow[52]+','+activerow[59]
+		if activerow[41].find("2_") > -1:
+			if activerow[52] == '1' or activerow[52] == '0':
+				if activerow[52] == '1':
+					rts_acc.append(int(activerow[59]))
+				else:
+					rts_inacc.append(int(activerow[59]))
+				activerow = activerow[1]+','+activerow[41]+','+activerow[52]+','+activerow[53]+','+activerow[59]
 				datarows.append(activerow)
-
-	rts.sort()
-	replacerts = []
-	trigger = 0
-	for i in range(len(rts[:6])-1,-1,-1):
-		if trigger > 0:
-			replacerts.append((rts[:6][i],trigger))
-		elif i < 0:
-			break	
-		elif rts[:6][i]-rts[:6][i-1] > 100:
-			trigger = rts[:6][i]
-	trigger = 0
-	for i in range(len(rts[-6:])):
-		if trigger > 0:
-			replacerts.append((rts[-6:][i],trigger))
-		elif i == len(rts[-6:])-1:
-			break
-		elif rts[-6:][i+1]-rts[-6:][i] > 100:
-			trigger = rts[-6:][i]
-
-	#print rts[:6]
-	#print rts[-6:]
-	#print "Replacing"
-	#print replacerts
+	
+	replacerts = id_outliers(rts_acc, 6, 100)
 
 	for row in datarows:
 		activerow = row.strip().split(',')
 		for rt in replacerts:
-			if int(activerow[3]) == rt[0]:
-				activerow[3] = unicode(str(rt[1]))
-		rtsbycase[activerow[1][:3]].append(int(activerow[3]))
+			if int(activerow[4]) == rt[0]:
+				activerow[4] = unicode(str(rt[1]))
+		rtsbycase[activerow[1][:3]].append(activerow)
 		activerow = ','.join(activerow)
-	
-	filereport = [filename[filename.rfind('/')+1:].strip(), datarows[0].strip().split(',')[0]]	
-	for case, rts in rtsbycase.iteritems():
-		print "Mean for "+case+" is: "+ str(numpy.mean(rts))
-		filereport.append(str(numpy.mean(rts)))
+	print "Number of trials was: " +  str(len(datarows))
+	filereport = [filename[filename.rfind('/')+1:].strip(), datarows[0].strip().split(',')[0], str(float(len(rts_acc))/float(len(datarows)))]	
+	for case, dataarray in rtsbycase.iteritems():
+		allrts = [int(datapoint[4]) for datapoint in dataarray]
+		correctrts = [int(datapoint[4]) for datapoint in dataarray if datapoint[2] == '1']
+		wrongrts = [int(datapoint[4]) for datapoint in dataarray if datapoint[2] == '0']
+#		hits = [int(datapoint[4]) for datapoint in dataarray if datapoint[2] == '1' and datapoint[3] == '4']
+#		falsealerts = [int(datapoint[4]) for datapoint in dataarray if datapoint[2] == '0' and datapoint == '4']
+		
+		print "Mean for "+case+" is: "+ str(numpy.mean(correctrts))
+		filereport.append(str(numpy.mean(correctrts)))
+		filereport.append(str(float(len(correctrts))/float(len(allrts))))
 	#	print rts
 	#	print "Size: "+str(len(rts))
 	datafile.close()
