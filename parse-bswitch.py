14b09c71d5773f6e6a4cccf1d6da3e76ef9662b0
Jordan <jordan@Elf.(none)>
2013-06-24 09:50:08 -0700

    Restructured both scripts to allow for batch processing of multiple datafiles, and updated bswitch script to process correct and incorrect response times separately

--- parse-bswitch.py
+++ parse-bswitch.py
@@ -1,73 +1,87 @@
 import codecs
 import numpy
-datafile = codecs.open('BSwitch - short - final-1-1.csv',encoding='utf16')
-datarows = []
-rts = []
-datarowsbycase = {'switch':[], 'nonswitch':[], 'all':[]}
-for row in datafile:
-	activerow = row.strip().split(',')
-	if activerow[82] == '1' or activerow[82] == '0':
-		rts.append(int(activerow[89]))
-		activerow = activerow[1]+','+activerow[72]+','+activerow[82]+','+activerow[89] #Subject, Cue Type (L/N), If Correct, Response Time
-		datarows.append(activerow)
+filelist = open('./bswitch-filelist')
+summary = [['Filename','Subject', '%ACC-switch', 'Mean RT-ACC_switch', 'Mean RT-INACC_switch', '%ACC-nonswitch', 'Mean RT-ACC-nonswitch', 'Mean RT-INACC_nonswitch', '%ACC-total', 'Mean RT-ACC_total', 'Mean RT-INACC_total']]
 
-rts.sort()
-replacerts = []
-trigger = 0
-for i in range(len(rts[:10])-1,-1,-1):
-	if trigger > 0:
-		replacerts.append((rts[:10][i],trigger))
-	elif i == 0:
-		break	
-	elif rts[:10][i]-rts[:10][i-1] > 100:
-		trigger = rts[:10][i]
-trigger = 0
-for i in range(len(rts[-10:])):
-	if trigger > 0:
-		replacerts.append((rts[-10:][i],trigger))
-	elif i == len(rts[-10:])-1:
-		break
-	elif rts[-10:][i+1]-rts[-10:][i] > 100:
-		trigger = rts[-10:][i]
+for filename in filelist:
+        print "Now processing " + filename.strip()
+        datafile = codecs.open(filename.strip(),encoding='utf16')
+	datarows = []
+	rts = []
+	datarowsbycase = {'switch':[], 'nonswitch':[], 'all':[]}
+        for row in datafile:
+                if row.count(',') < 80:
+                        continue
+		activerow = row.strip().split(',')
+		if activerow[82] == '1' or activerow[82] == '0':
+			rts.append(int(activerow[89]))
+			activerow = activerow[1]+','+activerow[72]+','+activerow[82]+','+activerow[89] #Subject, Cue Type (L/N), If Correct, Response Time
+			datarows.append(activerow)
 
-print rts[:10]
-print rts[-10:]
-print "Replacing"
-print replacerts
+	rts.sort()
+	replacerts = []
+	trigger = 0
+	for i in range(len(rts[:10])-1,-1,-1):
+		if trigger > 0:
+			replacerts.append((rts[:10][i],trigger))
+		elif i == 0:
+			break	
+		elif rts[:10][i]-rts[:10][i-1] > 100:
+			trigger = rts[:10][i]
+	trigger = 0
+	for i in range(len(rts[-10:])):
+		if trigger > 0:
+			replacerts.append((rts[-10:][i],trigger))
+		elif i == len(rts[-10:])-1:
+			break
+		elif rts[-10:][i+1]-rts[-10:][i] > 100:
+			trigger = rts[-10:][i]
 
-#print datarows
-priorstim = datarows[0].strip().split(',')[1]
-print priorstim
-#cleanfile = codecs.open('/tmp/out.csv','w',encoding='utf16')
-for i in range(0, len(datarows)):
-	activerow = datarows[i].strip().split(',')
-#	for rt in replacerts:
-#		if int(activerow[3]) == rt[0]:
-#			activerow[3] = unicode(str(rt[1]))
-	datarowsbycase['all'].append(activerow)
-	if activerow[1]  == priorstim:
-		datarowsbycase['nonswitch'].append(activerow)
-	else:
-		datarowsbycase['switch'].append(activerow)
-		priorstim = activerow[1]
+	print rts[:10]
+	print rts[-10:]
+	print "Replacing"
+	print replacerts
 
-#print datarowsbycase
-for case, dataarray in datarowsbycase.iteritems():
-	correctrts = [int(datapoint[3]) for datapoint in dataarray if datapoint[2] == '1']
-	wrongrts = [int(datapoint[3]) for datapoint in dataarray if datapoint[2] == '0']
-	#print correctrts
-	print "% accuracy for " + case + " trials: " + str(float(len(correctrts))/float(len(dataarray)))
-	print "% inaccuracy for " + case + " trials: " + str(float(len(wrongrts))/float(len(dataarray)))
-	print "Average response time for correct answers for " + case + " trials: " + str(numpy.mean(correctrts))
-	print "Average response time for wrong answers for " + case + " trials: " + str(numpy.mean(wrongrts))
+	#print datarows
+	priorstim = datarows[0].strip().split(',')[1]
+	#print priorstim
+	for i in range(0, len(datarows)):
+		activerow = datarows[i].strip().split(',')
+		for rt in replacerts:
+			if int(activerow[3]) == rt[0]:
+				activerow[3] = unicode(str(rt[1]))
+		datarowsbycase['all'].append(activerow)
+		if activerow[1]  == priorstim:
+			datarowsbycase['nonswitch'].append(activerow)
+		else:
+			datarowsbycase['switch'].append(activerow)
+			priorstim = activerow[1]
+
+	#print datarowsbycase
+	filereport = [filename[filename.rfind('/')+1:].strip(), datarows[0].strip().split(',')[0]]
+	for case, dataarray in datarowsbycase.iteritems():
+		correctrts = [int(datapoint[3]) for datapoint in dataarray if datapoint[2] == '1']
+		wrongrts = [int(datapoint[3]) for datapoint in dataarray if datapoint[2] == '0']
+		#print correctrts
+		print "% accuracy for " + case + " trials: " + str(float(len(correctrts))/float(len(dataarray)))
+		print "% inaccuracy for " + case + " trials: " + str(float(len(wrongrts))/float(len(dataarray)))
+		#print "Average response time for correct answers for " + case + " trials: " + str(numpy.mean(correctrts))
+		print "Average response time for wrong answers for " + case + " trials: " + str(numpy.mean(wrongrts))
+		filereport.append(str(float(len(correctrts))/float(len(dataarray))))
+		filereport.append(str(numpy.mean(correctrts)))
+		filereport.append(str(numpy.mean(wrongrts)))
 #	activerow = ','.join(activerow)
-#	cleanrows.append(activerow)
 #	print activerow
-#	cleanfile.write(activerow+"\n")
 # for case, rts in rtsbycase.iteritems():
 #	print "Mean for "+case+" is: "+ str(numpy.mean(rts))
 #	print rts
 #	print "Size: "+str(len(rts))
-#cleanfile.write("\n")
-#cleanfile.close()
+	print filereport
+        datafile.close()
+        summary.append(filereport)
+report = codecs.open('./bswitch-report.csv','w',encoding='utf16')
+for row in summary:
+        report.write(','.join(row) + "\n")
+report.write("\n")
+report.close();
 
--- parse-filtering.py
+++ parse-filtering.py
@@ -1,50 +1,66 @@
 import codecs
 import numpy
+filelist = open('./filtering-filelist')
+summary = [['Filename','Subject', '2_0 RT', '2_2 RT', '2_4 RT', '2_6 RT']]
+for filename in filelist:
+	print "Now processing " + filename.strip()
+	datafile = codecs.open(filename.strip(),encoding='utf16')
+	datarows = []
+	rts = []
+	rtsbycase = {'2_0':[], '2_2':[], '2_4':[], '2_6':[]}
+	for row in datafile:
+		if row.count(',') < 50:
+			continue
+		activerow = row.strip().split(',')
+		if activerow[52] == '1':
+			if activerow[41].find("2_") > -1:
+				rts.append(int(activerow[59]))
+				activerow = activerow[1]+','+activerow[41]+','+activerow[52]+','+activerow[59]
+				datarows.append(activerow)
 
-datafile = codecs.open('/tmp/Filtering-short-final-2-1.csv',encoding='utf16')
-datarows = []
-rts = []
-rtsbycase = {'2_0':[], '2_2':[], '2_4':[], '2_6':[]}
-for row in datafile:
-	activerow = row.strip().split(',')
-	if activerow[52] == '1':
-		if activerow[41].find("2_") > -1:
-			rts.append(int(activerow[59]))
-			activerow = activerow[1]+','+activerow[41]+','+activerow[52]+','+activerow[59]
-			datarows.append(activerow)
+	rts.sort()
+	replacerts = []
+	trigger = 0
+	for i in range(len(rts[:6])-1,-1,-1):
+		if trigger > 0:
+			replacerts.append((rts[:6][i],trigger))
+		elif i < 0:
+			break	
+		elif rts[:6][i]-rts[:6][i-1] > 100:
+			trigger = rts[:6][i]
+	trigger = 0
+	for i in range(len(rts[-6:])):
+		if trigger > 0:
+			replacerts.append((rts[-6:][i],trigger))
+		elif i == len(rts[-6:])-1:
+			break
+		elif rts[-6:][i+1]-rts[-6:][i] > 100:
+			trigger = rts[-6:][i]
 
-rts.sort()
-replacerts = []
-trigger = 0
-for i in range(len(rts[:6])-1,-1,-1):
-	if trigger > 0:
-		replacerts.append((rts[:6][i],trigger))
-	elif i == 0:
-		break	
-	elif rts[:6][i]-rts[:6][i-1] > 100:
-		trigger = rts[:6][i]
-trigger = 0
-for i in range(len(rts[-6:])):
-	if trigger > 0:
-		replacerts.append((rts[-6:][i],trigger))
-	elif i == len(rts[-6:])-1:
-		break
-	elif rts[-6:][i+1]-rts[-6:][i] > 100:
-		trigger = rts[-6:][i]
+	#print rts[:6]
+	#print rts[-6:]
+	#print "Replacing"
+	#print replacerts
 
-print rts[:6]
-print rts[-6:]
-print "Replacing"
-print replacerts
-
-for row in datarows:
-	activerow = row.strip().split(',')
-	for rt in replacerts:
-		if int(activerow[3]) == rt[0]:
-			activerow[3] = unicode(str(rt[1]))
-	rtsbycase[activerow[1][:3]].append(int(activerow[3]))
-	activerow = ','.join(activerow)
-#	print activerow
-for case, rts in rtsbycase.iteritems():
-	print "Mean for "+case+" is: "+ str(numpy.mean(rts))
-
+	for row in datarows:
+		activerow = row.strip().split(',')
+		for rt in replacerts:
+			if int(activerow[3]) == rt[0]:
+				activerow[3] = unicode(str(rt[1]))
+		rtsbycase[activerow[1][:3]].append(int(activerow[3]))
+		activerow = ','.join(activerow)
+	
+	filereport = [filename[filename.rfind('/')+1:].strip(), datarows[0].strip().split(',')[0]]	
+	for case, rts in rtsbycase.iteritems():
+		print "Mean for "+case+" is: "+ str(numpy.mean(rts))
+		filereport.append(str(numpy.mean(rts)))
+	#	print rts
+	#	print "Size: "+str(len(rts))
+	datafile.close()
+	#print filereport
+	summary.append(filereport)
+report = codecs.open('./filtering-report.csv','w',encoding='utf16')
+for row in summary:
+	report.write(','.join(row) + "\n")
+report.write("\n")
+report.close();
